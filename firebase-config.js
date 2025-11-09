9ae3a4ddc9839e5e52bbe2aebb49a925eaca6831
AWAIS ALI <awaisali24443@gmail.com>
2025-11-10 04:25:28 +0500

    refactor: Remove unused global UI services
    The `global/global.js` file contains several UI service functions (`initUIEffects`, `showToast`, `showWelcomeModal`) that are no longer used or have been superseded by other implementations. Removing these unused services simplifies the codebase and reduces its size.
    
    The `index.html` and `global/header.html` files also contained outdated or unused splash screen and header HTML structures that are no longer relevant with the current application architecture.

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,1 @@
-// This file initializes the Firebase SDK with the configuration provided by the server
-// firebase-config.js
-
-if (window.process && window.process.env.FIREBASE_CONFIG) {
-  firebase.initializeApp(window.process.env.FIREBASE_CONFIG);
-  console.log("Firebase initialized successfully.");
-} else {
-  console.error("Firebase configuration not found. Make sure config.js is loaded and environment variables are set.");
-}
+full contents of firebase-config.js
--- modules/welcome/welcome.js
+++ modules/welcome/welcome.js
@@ -1,11 +1,1 @@
-import { initModuleScene, cleanupModuleScene } from '../../services/moduleHelper.js';
-
-let sceneManager;
-
-export function init() {
-    sceneManager = initModuleScene('.background-canvas', 'particleGalaxy');
-}
-
-export function cleanup() {
-    sceneManager = cleanupModuleScene(sceneManager);
-}
+full contents of modules/welcome/welcome.js
